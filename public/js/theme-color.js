99c08ee3ddfc25f76ab2067626424ba9a21c65d7
RjM091309 <rjmanapsal@gmail.com>
2026-09-03 14:23:19 +0800

    updatedtoday1

--- public/js/theme-color.js
+++ public/js/theme-color.js
@@ -31,38 +31,26 @@ jQuery(document).ready(function() {
 
 	applySavedThemePreferences();
 
+	// The settings panel is now just this one Light/Dark control (the old
+	// Sidebar Color / Header Brand Color / Header Color swatch rows were
+	// removed from chat-sidebar.ejs), so this single handler now drives
+	// sidebar, logo AND header together - previously header was only ever
+	// set by its own now-removed swatch row, which would have left it stuck
+	// on the server's dark default forever in Light mode with no swatches
+	// left to fix it.
 	jQuery(document).on("click", ".theme-light-dark button", function() {
-        var logo_color = "logo-" + jQuery(this).attr('data-theme');
-        var sidebar_color = jQuery(this).attr('data-theme') + "-sidebar-color";
-        jQuery("body").removeClass("white-sidebar-color dark-sidebar-color blue-sidebar-color indigo-sidebar-color green-sidebar-color red-sidebar-color cyan-sidebar-color logo-white logo-dark logo-blue logo-indigo logo-red logo-cyan logo-green");
+        var theme = jQuery(this).attr('data-theme');
+        var sidebar_color = theme + "-sidebar-color";
+        var logo_color = "logo-" + theme;
+        var header_color = "header-" + theme;
+        jQuery("body").removeClass("white-sidebar-color dark-sidebar-color blue-sidebar-color indigo-sidebar-color green-sidebar-color red-sidebar-color cyan-sidebar-color logo-white logo-dark logo-blue logo-indigo logo-red logo-cyan logo-green header-white header-dark header-blue header-indigo header-red header-cyan header-green");
         jQuery("body").addClass(sidebar_color);
         jQuery("body").addClass(logo_color);
-        localStorage.setItem('sidebar_color', sidebar_color);
-        localStorage.setItem('logo_color', logo_color);
-        var selected = jQuery(this).attr('data-theme');
-        document.documentElement.setAttribute('data-theme', selected);
-        localStorage.setItem('ui_data_theme', selected);
-    });
-    jQuery(document).on("click", ".sidebar-theme a", function() {
-        var sidebar_color = jQuery(this).attr('data-theme') + "-sidebar-color";
-        jQuery("body").removeClass("white-sidebar-color dark-sidebar-color blue-sidebar-color indigo-sidebar-color green-sidebar-color red-sidebar-color cyan-sidebar-color");
-        jQuery("body").addClass(sidebar_color);
-        localStorage.setItem('sidebar_color', sidebar_color);
-        var selected = jQuery(this).attr('data-theme');
-        document.documentElement.setAttribute('data-theme', selected);
-        localStorage.setItem('ui_data_theme', selected);
-    });
-    jQuery(document).on("click", ".logo-theme a", function() {
-        var logo_color = jQuery(this).attr('data-theme');
-        jQuery("body").removeClass("logo-white logo-dark logo-blue logo-indigo logo-red logo-cyan logo-green");
-        jQuery("body").addClass(logo_color);
-        localStorage.setItem('logo_color', logo_color);
-    });
-    jQuery(document).on("click", ".header-theme a", function() {
-        var header_color = jQuery(this).attr('data-theme');
-        jQuery("body").removeClass("header-white header-dark header-blue header-indigo header-red header-cyan header-green");
         jQuery("body").addClass(header_color);
+        localStorage.setItem('sidebar_color', sidebar_color);
+        localStorage.setItem('logo_color', logo_color);
         localStorage.setItem('header_color', header_color);
-        // Do not change data-theme here; keep global theme driven by sidebar selection only
+        document.documentElement.setAttribute('data-theme', theme);
+        localStorage.setItem('ui_data_theme', theme);
     });
 });
